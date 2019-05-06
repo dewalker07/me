bfa2a39341ca0c4b9b15c2642eb87bd3dcdb386e
Diwakar <dabbara.divakar@gmail.com>
2019-05-06 19:02:14 +0530

    Updates

---
+++ precache-manifest.8f064042f12273e560a43a3b8f1a07af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "9a786e446b8a74e8ed4a",
+    "url": "/me/static/js/main.9a786e44.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "9a786e446b8a74e8ed4a",
+    "url": "/me/static/css/main.e69877f7.chunk.css"
+  },
+  {
+    "revision": "380e34a39aa7b117a73d4a55ef93b0dc",
+    "url": "/me/index.html"
+  }
+];
